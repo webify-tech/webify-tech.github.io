689c0f99b62ad659c7cc88726f5af89fefcf0015
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2024-10-09 22:36:40 +0300

    git add .

--- .next/static/chunks/pages/_app.js
+++ .next/static/chunks/pages/_app.js
@@ -5,7 +5,7 @@
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
+ */sss
 (self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["pages/_app"],{
 
 /***/ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js":
