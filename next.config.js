7bfbf54421842a03ef3cfd72fd738544cf16ff98
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2025-02-22 14:49:17 +0200

    rest next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,34 @@
-/** @type {import('next').NextConfig} */
-const isProd = true;
-const nextConfig = {
-  reactStrictMode: true, // Enables React's strict mode
-  images: {
-    domains: ['example.com'], // Allow loading images from specific domains
-  },
-  assetPrefix: isProd ? '/eg/' : '',
-  basePath: isProd ? '/eg' : '',
-  output: 'export'
-};
+// Enable this code below for Server Side Rendering/Translation (SSR)
+// const { i18n } = require('./next-i18next.config')
+const withImages = require('next-images');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = nextConfig;
+module.exports = withImages({
+  // Enable this code below for Server Side Rendering/Translation (SSR)
+  //  i18n,
+  output: 'export', // Please disable/comment for SSR Mode
+  trailingSlash: true,
+  images: {
+    disableStaticImages: true
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  publicRuntimeConfig: {
+    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+      ? process.env.LOCALE_SUBPATHS
+      : 'none',
+  },
+  webpack: (config, options) => {
+    cssModules: true,
+    config.plugins.push(
+      //      new ESLintPlugin({
+      //        exclude: ['node_modules']
+      //      })
+    );
+    config.node = {}
+    return config;
+  },
+});
