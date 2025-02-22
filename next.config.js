815ba8dd1bfead2fd77f1253b463cf1924c72e46
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2025-02-22 13:08:20 +0200

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Enables React strict mode
-  swcMinify: true, // Uses SWC for faster builds
-};
+// const { i18n } = require('./next-i18next.config')
+const withImages = require('next-images');
+// const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = nextConfig;
+module.exports = withImages({
+  // i18n,
+  trailingSlash: true,
+  images: {
+    disableStaticImages: true
+  },
+  publicRuntimeConfig: {
+    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+      ? process.env.LOCALE_SUBPATHS
+      : 'none',
+  },
+  webpack: (config, options) => {
+    cssModules: true,
+    config.plugins.push(
+      //      new ESLintPlugin({
+      //        exclude: ['node_modules']
+      //      })
+    );
+    config.node = {}
+    return config;
+  },
+});
