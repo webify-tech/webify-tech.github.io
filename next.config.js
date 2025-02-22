454fcfef40d033ef558e5c9cff5a7317d0ecd29d
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2025-02-22 12:52:18 +0200

    basic next config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
-// const { i18n } = require('./next-i18next.config')
-const withImages = require('next-images');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Enables React strict mode
+  swcMinify: true, // Uses SWC for faster builds
+};
 
-module.exports = withImages({
-  // i18n,
-  trailingSlash: true,
-  images: {
-    disableStaticImages: true
-  },
-  publicRuntimeConfig: {
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-      ? process.env.LOCALE_SUBPATHS
-      : 'none',
-  },
-  webpack: (config, options) => {
-    cssModules: true,
-    config.plugins.push(
-    );
-    config.node = {}
-    return config;
-  },
-});
+module.exports = nextConfig;
